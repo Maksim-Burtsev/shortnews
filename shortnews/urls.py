de183d6839d569c86c8c97272ac627716179205c
MaximBurtsev <zadrot-lol@list.ru>
2022-02-12 20:30:18 +0300

    add debug toolbar

--- shortnews/urls.py
+++ shortnews/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from shortnews import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls'))
 
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+    path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
