3f2f012b19f37b892cffa87f54f081db3d33cc22
MaximBurtsev <zadrot-lol@list.ru>
2022-02-16 13:20:10 +0300

    nyt patser update

--- news/parsers/nyt_tech_parse.py
+++ news/parsers/nyt_tech_parse.py
@@ -1,60 +1,45 @@
-import os
 import requests
 from bs4 import BeautifulSoup
-import json
 import datetime
 import sqlite3
 
-URL = 'https://www.nytimes.com/section/technology'
 
-def parser():
-    """Парсит данные и формирует из них словарь"""
+def parser() -> list:
+    """Парсит страницу и возвращает список заголовков и ссылок на статью"""
+
+    URL = 'https://www.nytimes.com/section/technology'
+
     response = requests.get(URL)
     soup = BeautifulSoup(response.text, 'lxml')
-    
-    section = soup.find('section', {'id' : 'stream-panel'})
 
-    li = section.find_all('li', {'class' : 'css-ye6x8s'})
-    
-    data = {}
+    section = soup.find('section', {'id': 'stream-panel'})
+
+    li = section.find_all('li', {'class': 'css-ye6x8s'})
+
+    res = []
+
     for i in range(len(li)):
         link = 'https://www.nytimes.com/' + li[i].find('a').get('href')
         title = li[i].find('h2').text
-        summary = li[i].find('p', {'class' : 'css-1echdzn e15t083i1'}).text
 
-        data[i] = {
-            'title' : title,
-            'summary' : summary,
-            'link' : link,
-        }
+        res.append((title, link))
 
-    return data
+    return res
 
-def make_json(data: dict):
-    """Формирует json из данных после парсинга"""
-
-    with open ('nyt_tech.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, indent=3, ensure_ascii=False)
-
-def get_data():
-    """Распаковывает json и формирует из него готовые строки для базы данных"""
-
-    with open('nyt_tech.json', 'r', encoding='utf-8') as f:
-        data = json.load(f)
-
+def pack_data(data: list):
+    """Формирует список кортежей для записи в базу данных"""
     
-    articles = []
-    news_id = 101
+    clean_data = []
+    db_id = 101
 
-    for i in range(10):
-        articles.append(
-            (data[str(i)]['title'], data[str(i)]['link'], datetime.datetime.now(), news_id))
-        
-        news_id += 1
+    for article in data:
+        clean_data.append((article[0], article[1], datetime.datetime.now(), db_id))
 
-    return articles
+        db_id += 1
 
-def update_database():
+    return clean_data
+
+def update_db(data):
     """Обновляет базу данных"""
 
     try:
@@ -66,20 +51,21 @@ def update_database():
     cursor = sqlite_connection.cursor()
 
     sql_update_query = """Update news_news set title = ?, link = ?, time_created = ? where id = ?"""
-    articles = get_data()
-    cursor.executemany(sql_update_query, articles)
+
+    cursor.executemany(sql_update_query, data)
     
     sqlite_connection.commit()
     cursor.close()
 
     sqlite_connection.close()
 
-    os.remove('nyt_tech.json')
 
 def nyt_tech_main():
     data = parser()
-    make_json(data)
-    update_database()
+    
+    clean_data = pack_data(data)
+
+    update_db(clean_data)
 
 if __name__ == '__main__':
     nyt_tech_main()
