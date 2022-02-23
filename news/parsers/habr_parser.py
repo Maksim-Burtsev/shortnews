c6a986bd58d03d3cfc5ade6a56e88793914b8df3
MaximBurtsev <zadrot-lol@list.ru>
2022-02-23 10:48:34 +0300

    fix parser

--- news/parsers/habr_parser.py
+++ news/parsers/habr_parser.py
@@ -2,30 +2,60 @@ import requests
 from bs4 import BeautifulSoup
 import sqlite3
 import datetime
+import fake_useragent
+
 
 URL = 'https://habr.com/ru/all/'
 
-def clean_data(titles: list, links: list, post_id: int) -> list:
-    """Получает на вход списки названия и ссылок статей и формирует готовые кортежи для записи в БД"""
-    time_created = datetime.datetime.now()
 
-    res = []
+def get_articles_from_page(url) -> list:
+    """Парсит все статьи со страницы"""
 
-    for i in range(len(titles)):
-        tmp = (titles[i], links[i],
-               time_created, post_id)
-        post_id += 1
-        res.append(tmp)
+    user = fake_useragent.UserAgent().random
+    header = {
+        'user-agent': user,
+    }
 
-    return res
+    response = requests.get(url, headers=header)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    div = soup.find('div', {'class': 'tm-articles-subpage'})
+
+    articles = div.find_all('article')
+
+    return articles
+
+
+def get_post_data(article, post_id: int) -> tuple:
+    """Достаёт из поста всю необходимую информацию и упаковывает её для записи в БД"""
+
+    try:
+        title = article.find(
+            'a', {'class': 'tm-article-snippet__title-link'}).text
+    except:
+        return False
+
+    time = datetime.datetime.strptime(
+        article.find('time').get('title'),
+        '%Y-%m-%d, %H:%M',
+    )
+
+    url = 'https://habr.com' + \
+        article.find(
+            'a', {'class': 'tm-article-snippet__title-link'}).get('href')
+
+    return (title, url, time, post_id)
+
 
 def update_database(data: list[tuple]):
     """Обновляет базу данных"""
 
     try:
-       sqlite_connection = sqlite3.connect("E:\shortnews\shortnews\db.sqlite3")
+        sqlite_connection = sqlite3.connect(
+            "E:\shortnews\shortnews\db.sqlite3")
     except:
-        sqlite_connection = sqlite3.connect("C:\\Users\\user\\h_w\\shortnews\\db.sqlite3")
+        sqlite_connection = sqlite3.connect(
+            "C:\\Users\\user\\h_w\\shortnews\\db.sqlite3")
     cursor = sqlite_connection.cursor()
 
     sql_update_query = """Update news_news set title = ?, link = ?, time_created = ? where id = ?"""
@@ -36,41 +66,28 @@ def update_database(data: list[tuple]):
 
     sqlite_connection.close()
 
-def parse(url: str):
-    """Парсит со страницы все названия статей и ссылки на них"""
-
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-
-    div = soup.find('div', {'class': 'tm-articles-list'})
-
-    articles = div.find_all('article', {'class': 'tm-articles-list__item'})
-
-    titles = []
-    links = []
-
-    for article in articles:
-        a = article.find('a', {'class': 'tm-article-snippet__title-link'})
-        titles.append(a.text)
-        links.append('https://habr.com' + a.get('href'))
-
-    return titles, links
-
 
 def habr_parser_main():
     """Основная функция программы"""
-    
-    titles, links = [], []
+
+    URL = 'https://habr.com/ru/all/page'
+    all_data = []
+    post_id = 131
 
     for i in range(1, 6):
-        url = URL + f'page{i}/'
-        title, link = parse(url)
+        url = URL + str(i) + '/'
+        articles = get_articles_from_page(url)
+        for j in range(len(articles)):
+            data = get_post_data(articles[j], post_id)
+            if data:
+                all_data.append(data)
+                post_id += 1
+            else:
+                pass
+            print(f'article{j+1}/{len(articles)}')
+        print(f'page {i}/5')
 
-        titles.extend(title)
-        links.extend(link)
-
-    data = clean_data(titles, links, 131)
-    update_database(data)
+        update_database(all_data)
 
 
 if __name__ == '__main__':
---
+++ wiki/parser/wiki.py
@@ -0,0 +1,78 @@
+import wikipedia
+import sqlite3
+
+
+EXTENSIONS = ('.jpg', 'jpeg', '.png', '.gif')
+SKIP_IMAGES = (
+    'https://upload.wikimedia.org/wikipedia/commons/b/b4/Red_pog.png',
+    'https://upload.wikimedia.org/wikipedia/commons/e/e4/Jupiter_and_moon.png',
+    'https://upload.wikimedia.org/wikipedia/commons/6/65/Image-silk.png'
+)
+wikipedia.set_lang('ru')    
+
+
+def get_image(images: list) -> str:
+    """Получает список картинок за страницы и возвращает первую с приемлемым расширением"""
+
+    for image in images:
+        if image[-4:] in EXTENSIONS and image not in SKIP_IMAGES:
+            return image
+    return ''
+
+
+def get_data(titles: list) -> list[tuple]:
+    """Собирает и формирует всю необходимюу информацию о статьях для записи в БД"""
+    ID = 6
+
+    res = []
+    for title in titles:
+        try:
+            page = wikipedia.page(title)
+
+        except Exception as e:
+            print(e)
+
+        else:
+            summary = page.summary
+            url = page.url
+            image_link = get_image(page.images)
+
+            res.append((title, summary, url, image_link, ID))
+
+            ID += 1
+
+    return res
+
+
+def update_db(data):
+    """Обновляет базу данных"""
+
+    try:
+        sqlite_connection = sqlite3.connect(
+            "E:\shortnews\shortnews\db.sqlite3")
+    except:
+        sqlite_connection = sqlite3.connect(
+            "C:\\Users\\user\\h_w\\shortnews\\db.sqlite3")
+    cursor = sqlite_connection.cursor()
+
+    sql_update_query = """Update wiki_article set title = ?, summary = ?, url = ?, image_link = ? where id = ?"""
+    cursor.executemany(sql_update_query, data)
+
+    sqlite_connection.commit()
+    cursor.close()
+
+    sqlite_connection.close()
+
+
+def main():
+
+    titles = wikipedia.random(pages=39)
+    data = get_data(titles)
+
+    update_db(data)
+
+    print(data[0])
+
+
+if __name__ == '__main__':
+    main()
