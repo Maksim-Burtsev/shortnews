f27de65aff43656874ba7742ec0d8718c63934d7
MaximBurtsev <zadrot-lol@list.ru>
2022-03-02 12:35:17 +0300

    add tests

--- news/tests/test_views.py
+++ news/tests/test_views.py
@@ -1,27 +1,89 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-from news.models import News, Category, Currency
+from news.models import News, Category
 
 
 class NewsViewsTest(TestCase):
 
     def test_index(self):
-        pass
+        response = self.client.get(reverse('home'))
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertTemplateUsed(response, 'news/index.html')
 
     def test_show_category(self):
-        pass
 
-    def test_search(self):
-        pass
+        cat = Category.objects.create(
+            name='Тестовая категория',
+            slug='test',
+            link='https://test_link.com',
+        )
+        for _ in range(10):
+            news = News.objects.create(
+                title='Тестовая новость',
+                link='https://google.com',
+                cat_id=1,
+            )
+
+        response = self.client.get(reverse('category', args=('test',)))
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertTemplateUsed(response, 'news/more.html')
 
     def test_hide(self):
-        pass
+        cat = Category.objects.create(
+            name='Тестовая категория',
+            slug='test',
+            link='https://test_link.com',
+        )
+        for _ in range(10):
+            news = News.objects.create(
+                title='Тестовая новость',
+                link='https://google.com',
+                cat_id=1,
+            )
 
-    def test_register(self):
-        pass
+        response = self.client.get(
+            reverse('hide post', args=(1,)), follow=True)
 
-    def test_autorize(self):
-        pass
+        self.assertEqual(response.status_code, 200)
 
-    def test_logout(self):
-        pass
+        news = News.objects.get(pk=1)
+
+        self.assertFalse(news.is_published)
+
+    def test_register_POST(self):
+
+        response = self.client.post(reverse('register'),
+                                    {
+            'username': 'vasiliy',
+            'password1': 'JSW3f6i-pcLgsKK',
+            'password2': 'JSW3f6i-pcLgsKK',
+        },
+            follow=True
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_GET(self):
+
+        response = self.client.get(reverse('register'))
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertTemplateUsed(response, 'news/register.html')
+
+    def test_autorize_get(self):
+
+        response = self.client.get(reverse('authorization'))
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertTemplateUsed(response, 'news/autorize.html')
+
+
+
